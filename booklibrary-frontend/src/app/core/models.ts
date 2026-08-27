245a6848e96f0443008c3b08ae4dd6c1df1ed1d3
George Cristian Ciolponea <georgeciolponea@outlook.com>
2026-08-27 09:39:09 +0200

    Created and added authservice login method and scaffolded the login components

---
+++ booklibrary-frontend/src/app/core/models.ts
@@ -0,0 +1,6 @@
+export interface AuthResponse {
+    id : number;
+    username: string;
+    email : string;
+    token : string;
+}
---
+++ booklibrary-frontend/src/app/core/services/auth.service.ts
@@ -0,0 +1,27 @@
+import {Injectable, signal, computed, inject} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AuthResponse } from '../models';
+import { tap } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+
+    private tokenSignal = signal<string | null>(null);
+    private usernameSignal = signal<string | null>(null);
+    readonly isLoggedIn = computed(() => this.tokenSignal() !== null);
+
+    private http = inject(HttpClient);
+
+    login(email: string, password:string){
+        return this.http.post<AuthResponse>('http://localhost:5222/api/Auth/login', 
+            {email, password}).pipe(
+            tap((response : AuthResponse) => {
+                this.tokenSignal.set(response.token);
+                this.usernameSignal.set(response.username);
+            })
+        );
+    }
+}
+
